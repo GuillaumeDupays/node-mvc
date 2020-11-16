8a13b11a553cf07a6a04b559fbe0e49a160c11e2
GuillaumeDupays <guillaumedupays@gmail.com>
2020-11-16 08:05:47 +0100

    .populate articles

--- controleurs/article.js
+++ controleurs/article.js
@@ -2,17 +2,15 @@
 const Article = require('../modeles/article');
 
 exports.creerArticle = (req, res, next) => {
-    const articleObject = JSON.parse(req.body);
-    const article = new Article({
-        ...articleObject
-    });
+    // const articleObject = JSON.parse(req.body);
+    const article = new Article(req.body);
     article.save()
         .then(() => res.status(201).json({ message: 'Article enregistré' }))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifierArticler = (req, res, next) => {
-    Article.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    Article.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id }, { auteur: req.auteur._id})
         .then(() => res.status(200).json({ message: 'Article modifié'}))
         .catch(error => res.status(400).json({ error }));
 };
@@ -25,6 +23,7 @@ exports.supprimerArticle = (req, res, next) => {
 
 exports.getOneArticle = (req, res, next) => {
     Article.findOne({ _id: req.params.id })
+        .populate('auteur')
         .then(article => res.status(200).json(article))
         .catch(error => res.status(404).json({ error }));
 };
--- modeles/article.js
+++ modeles/article.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const articleSchema = mongoose.Schema({
+const articleSchema = new Schema({
     titre: {type: String, required: true},
     description: {type: String, required: true},
+    auteur: {
+        type: Schema.Types.ObjectId,
+        ref: 'Auteur'
+    }
 /*    userId: String*/
 });
 
